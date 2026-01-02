f5ddbbe7f85fa63ab407aa98bb8b5c9225f57d53
Sahil Bhola <sbhola@umich.edu>
2026-01-02 18:32:40 -0500

    feat: add beta distribution sampler

--- src/cpp/distribution.cpp
+++ src/cpp/distribution.cpp
@@ -2,16 +2,14 @@
 
 #include <cuda_fp16.h>
 
+#include <cassert>
 #include <iostream>
 #include <random>
 #include <stdexcept>
 
 /* sample from N(0,1) */
 template <typename T>
-void sample_normal_vector(std::vector<T> &vector, Precision prec,
-                          const int seed = 42) {
-  /* seed */
-  std::mt19937 gen(seed);
+void sample_normal_vector(std::vector<T> &vector, std::mt19937 &gen) {
   /* normal distribution (double)*/
   std::normal_distribution<double> normal(0.0, 1.0);
   /* sample random vector */
@@ -23,10 +21,7 @@ void sample_normal_vector(std::vector<T> &vector, Precision prec,
 /* sample from U(lower, upper) */
 template <typename T>
 void sample_uniform_distribution(std::vector<T> &vector, const double lower,
-                                 const double upper, Precision prec,
-                                 const int seed = 42) {
-  /* seed */
-  std::mt19937 gen(seed);
+                                 const double upper, std::mt19937 &gen) {
   /* uniform distribution (double)*/
   std::uniform_real_distribution<double> uniform(lower, upper);
   /* sample random vector */
@@ -35,10 +30,26 @@ void sample_uniform_distribution(std::vector<T> &vector, const double lower,
   }
 }
 
+/* sample from Beta distribution(alpha,beta) */
+template <typename T>
+void sample_beta_distribution(std::vector<T> &vector, const double alpha,
+                              const double beta, std::mt19937 &gen) {
+  /* gamma distribution */
+  assert(alpha > 0 && "alpha value must be strictly positive");
+  assert(beta > 0 && "beta value must be strictly positive");
+  std::gamma_distribution<double> dist_a(alpha, 1.0);
+  std::gamma_distribution<double> dist_b(beta, 1.0);
+  /* sample random vector */
+  for (auto &x : vector) {
+    double a = dist_a(gen);
+    double b = dist_b(gen);
+    x = static_cast<T>(a / (a + b));
+  }
+}
+
 /* constnat initialization */
 template <typename T>
-void constant_initialization(std::vector<T> &vector, const double constant,
-                             Precision prec) {
+void constant_initialization(std::vector<T> &vector, const double constant) {
   /* initialize */
   for (auto &x : vector) {
     x = static_cast<T>(constant);
@@ -49,17 +60,18 @@ void constant_initialization(std::vector<T> &vector, const double constant,
 template <typename T>
 void sample_random_vector(std::vector<T> &vector, Precision prec,
                           Distribution dist, const int pow2k, const int seed) {
+  std::mt19937 gen(seed);
   if (dist == Normal) {
-    sample_normal_vector(vector, prec, seed);
+    sample_normal_vector(vector, gen);
   } else if (dist == ZeroOne) {
-    sample_uniform_distribution(vector, 0.0, 1.0, prec, seed);
+    sample_uniform_distribution(vector, 0.0, 1.0, gen);
   } else if (dist == MinusOnePlusOne) {
-    sample_uniform_distribution(vector, -1.0, 1.0, prec, seed);
+    sample_uniform_distribution(vector, -1.0, 1.0, gen);
   } else if (dist == PowTwo) {
     sample_uniform_distribution(vector, std::pow(2.0, pow2k),
-                                std::pow(2.0, pow2k + 1), prec, seed);
+                                std::pow(2.0, pow2k + 1), gen);
   } else if (dist == Ones) {
-    constant_initialization(vector, 1.0, prec);
+    constant_initialization(vector, 1.0);
   } else {
     throw std::invalid_argument("Invalid distribution");
   }
